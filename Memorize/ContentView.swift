0946ff8631cd499c569145cd31fb48a44a416ca5
appcoder12 <puriprasoon@gmail.com>
2024-11-07 15:06:21 +0400

    Asignment 1 Complete

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: Array<String> = ["😀", "👻", "🎃", "🤡", "🤖","🤡"]
+    let emojis: Array<String> = ["😀", "👻", "🎃", "🤡", "🤖","💩","👽","🤠","💀"]
+    @State var cardCount: Int = 4
     var body: some View {
-        HStack(content: {
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
+        return HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+            
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offSet: Int, symbol: String) -> some View {
+        Button(action: {
+                cardCount += offSet
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offSet < 1 || cardCount + offSet > emojis.count)
+    }
+    
+    var cards: some View {
+        
+    return LazyVGrid(columns: [GridItem(), GridItem()], content: {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index], isFaceUp: true)
+                    .aspectRatio(2/3, contentMode: .fill)
             }
         })
+        .foregroundColor(.orange)
+    }
+    var cardRemover: some View {
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    var cardAdder: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
     }
 }
-
 
 struct CardView: View {
     let content: String
@@ -25,17 +65,25 @@ struct CardView: View {
     var body: some View {
         ZStack(content: {
             let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12.0)
-            if isFaceUp {
+            //            if isFaceUp {
+            //                Text(content)
+            //                    .font(.largeTitle)
+            //                base.strokeBorder(lineWidth: 2)
+            //            } else {
+            //                Text(content)
+            //                    .font(.largeTitle)
+            //                    .opacity(1)
+            //                base.strokeBorder(lineWidth: 2)
+            //                    .fill()
+            //            }
+           
+            Group {
+                base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                    .fill(.white)
-                Text(content)
-                    .font(.largeTitle)
-            } else {
-                
-                base.strokeBorder(lineWidth: 2)
-                    .fill()
-                    .foregroundColor(.orange)
+                Text(content).font(.largeTitle)
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }).onTapGesture {
             isFaceUp.toggle()
         }
